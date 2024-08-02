2c78548ddf2ef511cfda1a53cc487298b2c41ca9
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-08-02 12:01:38 +0300

    Add: Controllers for Instructor

---
+++ MVC/MVC_Project/MVC_Project/Controllers/InstructorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Context;
+using MVC_Project.ViewModels;
+
+namespace MVC_Project.Controllers
+{
+    public class InstructorController : Controller
+    {
+        SchoolContext db = new SchoolContext();
+        public IActionResult GetAll()
+        {
+            var depts = db.Instructors.ToList();
+            return View(depts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var depts = db.Instructors
+                .Include(d => d.Department)
+                .Include(c => c.Course)
+                .FirstOrDefault(d => d.InstId == id);
+
+            return View(depts);
+
+        }
+    }
+}
