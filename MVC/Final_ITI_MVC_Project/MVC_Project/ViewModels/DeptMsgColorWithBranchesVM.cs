7634c7bb9f188eebf1323cd3ca4d7886a75778ed
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-08-09 16:04:22 +0300

    View models for all branches in department

---
+++ MVC/Final_ITI_MVC_Project/MVC_Project/ViewModels/DeptMsgColorWithBranchesVM.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_Project.ViewModels
+{
+    public class DeptMsgColorWithBranchesVM
+    {
+        public string Department { get; set; }
+        public string Manager { get; set; }
+        public string Message { get; set; }
+        public string Color { get; set; }
+        public int TraineesNumber { get; set; }
+        public List<string> Branches { get; set; } = new List<string>();
+    }
+}
