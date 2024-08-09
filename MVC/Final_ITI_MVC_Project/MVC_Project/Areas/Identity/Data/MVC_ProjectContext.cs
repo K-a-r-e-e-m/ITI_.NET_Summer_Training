b5654212df9e5e2ffa25c3019a4203dec0f04f15
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-08-09 15:59:02 +0300

    Add areas for identity framework

---
+++ MVC/Final_ITI_MVC_Project/MVC_Project/Areas/Identity/Data/MVC_ProjectContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC_Project.Areas.Identity.Data;
+
+namespace MVC_Project.Areas.Identity.Data;
+
+public class MVC_ProjectContext : IdentityDbContext<MVC_ProjectUser>
+{
+    public MVC_ProjectContext(DbContextOptions<MVC_ProjectContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<MVC_ProjectUser>
+{
+    public void Configure(EntityTypeBuilder<MVC_ProjectUser> builder)
+    {
+        builder.Property(n => n.FristName).HasMaxLength(100);
+        builder.Property(n => n.LastName).HasMaxLength(100);
+    }
+}
