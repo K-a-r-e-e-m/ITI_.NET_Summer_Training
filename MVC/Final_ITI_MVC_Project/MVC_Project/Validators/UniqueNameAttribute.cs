527dab9e60a2d9466c5961d45f5866ead3a48d2a
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-08-09 16:03:43 +0300

    Add: add more validation on property

---
+++ MVC/Final_ITI_MVC_Project/MVC_Project/Validators/UniqueNameAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using MVC_Project.Context;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Project.Validators
+{
+    public class UniqueNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string name = value as string;
+            SchoolContext db = new SchoolContext();
+            var tran = db.Trainees.FirstOrDefault(t => t.Name == name);
+            if (tran == null)
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Name already exists!");
+        }
+    }
+}
