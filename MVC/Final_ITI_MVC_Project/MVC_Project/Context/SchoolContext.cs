adfe1a8fb53081a0d0cbb7e562acc10d80e656a5
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-08-09 15:59:29 +0300

    Add: Context of project school database

---
+++ MVC/Final_ITI_MVC_Project/MVC_Project/Context/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.Areas.Identity.Data;
+using MVC_Project.Models;
+
+namespace MVC_Project.Context
+{
+    public class SchoolContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = KAREEM\\SQLEXPRESS; Database = MVC_Project_ITI; Trusted_Connection = true; Encrypt = false");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Trainee> Trainees { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CrsResult> CrsResults { get; set; }
+        public DbSet<MVC_ProjectUser> MVCProjectUsers { get; set; }
+    }
+}
